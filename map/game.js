6bdd20ecdb06bec63d2377e83153e932ea0e7054
fortythreesam <samdrugan@gmail.com>
2016-10-17 21:14:13 +0100

    Space game

--- map/game.js
+++ map/game.js
@@ -1,9 +1,23 @@
 (function (){
-	var map = []
-	var new_map = []
-	var n = 0;
+
+	var canvas
+	var context
+	var game_state = 1;
+	var player = {
+		territories:[],
+	}
+	var map = [];
+	var new_map = [];
 	var land_id = 0;
+	var lands = [];
+	var smallest_x;
+	var smallest_y;
+	var largest_x;
+	var largest_y;
 	var tiles;
+	var free_lands = [];
+	var mouse_x;
+	var mouse_y;
 	document.addEventListener('DOMContentLoaded', init, false);
   
     function init(){
@@ -15,23 +29,45 @@
 			map.push([])
 			new_map.push([])
 			for(var j = 0; j < 200; j += 1){
-				map[i].push(0)
-				new_map[i].push(0)
+				map[i].push(-1)
+				new_map[i].push(-1)
 			}
 		}
 	
-		generateMap()
+		generateMap();
+		
 		draw()
+		window.setInterval(main,"16");
+		window.addEventListener("mousemove", getPosition, false);
     }
+	
+	function main(){
+		/*if (mouse_x > 0 && mouse_x < 1000 && mouse_y > 0 && mouse_y < 800){
+			var mouse_id = map[Math.floor(mouse_x/4)][Math.floor(mouse_y/4)]
+			if (mouse_id >= 0){
+				for (var i = lands[mouse_id].start_x; i <= lands[mouse_id].end_x; i +=1){
+					for (var j = lands[mouse_id].start_y; j <= lands[mouse_id].end_y; j += 1){
+						if (map[i][j] === mouse_id){
+							context.fillStyle = "#cc2222"
+							context.fillRect(i*4,j*4,4,4);
+						}
+					}
+				}
+			}
+		}*/
+		if (game_state === 1){
+			
+		}
+	}
 	
 	function draw(){
 		for (var i = 0; i < 250; i +=1){
 			for (var j = 0; j < 200; j += 1){
-				if (map[i][j] === 0) {
+				if (map[i][j] === -1) {
 					context.fillStyle = "rgb("+grn(40,50)+","+grn(130,135)+","+grn(165,195)+")";
 					context.fillRect(i*4,j*4,4,4);
 				}
-				else if (map[i][j] === -1){
+				else if (map[i][j] === -2){
 					context.fillStyle = "#222222"
 					context.fillRect(i*4,j*4,4,4);
 				}
@@ -62,21 +98,21 @@
 					var empty_y = grn(15,185)
 					for (var a = empty_x - grn(5,15);a < empty_x + grn(5,15);a += 1){
 						for (var b = empty_y - grn(5,15);b < empty_y + grn(5,15);b += 1){
-							map[a][b] = 0
+							map[a][b] = -1
 						}
 					}
 				}
 			}
 			for (var i = 2; i < 248; i +=1){
 				for (var j = 2; j < 198; j += 1){
-					if (map[i][j] > 0){
+					if (map[i][j] > -1){
 						new_map[i][j] = map[i][j]
 						if (checkSurrounding(i,j)){
 							var keepgoing = true;
 							while (keepgoing){
 								var check_x = grn(i-1,i+1);
 								var check_y = grn(j-1,j+1);
-								if (map[check_x][check_y] === 0){
+								if (map[check_x][check_y] === -1){
 									new_map[check_x][check_y] = map[i][j];
 									keepgoing = false;
 								}
@@ -91,14 +127,14 @@
 			for (var i = 0; i < 250; i +=1){
 				new_map.push([])
 				for(var j = 0; j < 200; j += 1){
-					new_map[i].push(0)
+					new_map[i].push(-1)
 				}
 			}
 		}
 		for (var n = 0; n < 5; n += 1){
 			for (var i = 1; i < 249; i +=1){
 				for (var j = 1; j < 199; j += 1){
-					if (map[i][j] > -1){
+					if (map[i][j] > -2){
 						if (checkNeighbours(i,j) <= 2){
 							if (map[i][j] !== map[i-1][j]){
 								map[i][j] = map[i-1][j]
@@ -120,9 +156,9 @@
 		//adding borders
 		for (var i = 1; i < 249; i +=1){
 			for (var j = 1; j < 199; j += 1){
-				if (map[i][j] > 0){
+				if (map[i][j] > -1){
 					if (checkBorders(i,j) < 3){
-						new_map[i][j] = -1;
+						new_map[i][j] = -2;
 					}
 					else{
 						new_map[i][j] = map[i][j]
@@ -136,7 +172,7 @@
 		for (var n = 0; n < 5; n += 1){
 			for (var i = 1; i < 249; i +=1){
 				for (var j = 1; j < 199; j += 1){
-					if (map[i][j] > -1){
+					if (map[i][j] > -2){
 						if (checkNeighbours(i,j) <= 2){
 							if (map[i][j] !== map[i-1][j]){
 								map[i][j] = map[i-1][j]
@@ -159,17 +195,33 @@
 		for (var j = 1; j < 199; j +=1){
 			for (var i = 1; i < 249; i += 1){
 				if (map[i][j] > 90){
+					smallest_x = 200;
+					largest_x = 0;
+					largest_y = 0;
 					tiles = 0
-					land_id += 1
+					smallest_y = j; 
 					floodfill(i,j,map[i][j]);
 					if (tiles < 200){
-						for (var x = 1; x < 249; x +=1){
-							for (var y = 1; y < 199; y += 1){
+						for (var x = smallest_x; x <= largest_x; x +=1){
+							for (var y = smallest_y; y <= largest_y; y += 1){
 								if (map[x][y] === land_id){
-									map[x][y] = 0;
+									map[x][y] = -1;
 								}
 							}
 						}
+					}
+					else{
+						var land = {
+							id:land_id,
+							start_x:smallest_x,
+							start_y:smallest_y,
+							end_x:largest_x,
+							end_y:largest_y,
+							size:tiles,
+						}
+						lands.push(land);
+						free_lands.push(land_id);
+						land_id += 1
 					}
 				}
 			}
@@ -177,26 +229,43 @@
 		for (var n = 0; n < 5; n += 1){
 			for (var i = 1; i < 249; i +=1){
 				for (var j = 1; j < 199; j += 1){
-					if (map[i][j] === -1){
+					if (map[i][j] === -2){
 						var remove = true;
 						for (var a = i-1 ;a < i+2; a += 1){
 							for (var b = j-1 ;b < j+2; b += 1){
-								if (map[a][b] !== 0 && map[a][b] !== -1){
+								if (map[a][b] !== -1 && map[a][b] !== -2){
 									remove = false;
 								}
 							}
 						}
 						if (remove){
-							map[i][j] = 0;
+							map[i][j] = -1;
 						}
 					}
 				}
 			}
 		}
 	}
+
+	function generate_player(){
+		var land_pos = grn(0,free_lands.length)
+		player.territories.push(free_lands[land_pos])
+		free_lands.splice(land_pos)
+		
+		
+	}
 	
 	function floodfill(x,y,number){
 		if (map[x][y] === number){
+			if (x > largest_x){
+				largest_x = x;
+			}
+			if (y > largest_y){
+				largest_y = y;
+			}
+			if (x < smallest_x){
+				smallest_x = x;
+			}
 			map[x][y] = land_id;
 			tiles += 1
 			floodfill(x+1,y,number)
@@ -206,11 +275,18 @@
 		}
 	}
 	
+	function getPosition(event){
+		mouse_x = event.x;
+		mouse_y = event.y;
+		
+		mouse_x -= canvas.offsetLeft;
+		mouse_y -= canvas.offsetTop;
+	}
+	
 	function checkSurrounding(x,y){
-		var total = 0
 		for (var a = x-1 ;a < x+2; a += 1){
 			for (var b = y-1 ;b < y+2; b += 1){
-				if (map[a][b] === 0){
+				if (map[a][b] === -1){
 					return true
 				} 
 			}
@@ -222,7 +298,7 @@
 		var total = -1
 		for (var a = x-1 ;a < x+2; a += 1){
 			for (var b = y-1 ;b < y+2; b += 1){
-				if (map[a][b] === map[x][y] || new_map[a][b] === -1 ){
+				if (map[a][b] === map[x][y] || new_map[a][b] === -2 ){
 					total += 1
 				}
 			}
@@ -232,16 +308,16 @@
 	
 	function checkBorders(x,y){
 		var total = -1
-		if (map[x+1][y] === map[x][y] || new_map[x+1][y] === -1 ){
+		if (map[x+1][y] === map[x][y] || new_map[x+1][y] === -2 ){
 				total += 1
 		}
-		if (map[x-1][y] === map[x][y] || new_map[x-1][y] === -1 ){
+		if (map[x-1][y] === map[x][y] || new_map[x-1][y] === -2 ){
 				total += 1
 		}
-		if (map[x][y+1] === map[x][y] || new_map[x][y+1] === -1 ){
+		if (map[x][y+1] === map[x][y] || new_map[x][y+1] === -2 ){
 				total += 1
 		}
-		if (map[x][y-1] === map[x][y] || new_map[x][y-1] === -1 ){
+		if (map[x][y-1] === map[x][y] || new_map[x][y-1] === -2 ){
 				total += 1
 			}
 		return total			
@@ -259,6 +335,6 @@
     function grn(min, max) {
 		return Math.floor(Math.random() * (max - min+1)) + min;
     }
-  
+	
 
 })(); 
--- space_game/game.js
+++ space_game/game.js
@@ -4,20 +4,27 @@
 			x: 30,
 			y: 30,
 			speed: 0,
-			max_speed: 25,
-			acceleration: 0.3,
+			x_speed:0,
+			y_speed:0,	
+			max_speed: 50,
+			acceleration: 0.7,
+			decceleration:0.1,
 			handling: 0.10,
 			moving_direction: 0,
 			facing_direction:0
 		};
 	var vehicle_image = new Image();
-	vehicle_image.src = "ship_image.png";
+	vehicle_image.src = "images/ship_image.png";
+	var bullet_image = new Image();
+	bullet_image.src = "images/bullet_image.png";
+	var bullets = []
+	var bullet_timer = 0;
 	var image_index = 0;
 	var accelerating = false;
 	var clockwise = false;
 	var counterclockwise = false;
 	var reverse = false;
-	var gate
+	var shooting = false;
 
     document.addEventListener('DOMContentLoaded', init, false);
   
@@ -32,23 +39,26 @@
     }
 	
 	function update(){
-		if (reverse){
-			vehicle.speed -= 1;
-		}
 		if (accelerating && reverse === false){
-			if (vehicle.speed >= vehicle.max_speed){
-				vehicle.speed = vehicle.max_speed;
+			if (Math.abs(vehicle.x_speed) >= Math.abs(vehicle.max_speed * Math.cos(vehicle.facing_direction))){
+				vehicle.x_speed -= Math.sign(vehicle.x_speed)-1*vehicle.acceleration * Math.cos(vehicle.facing_direction)
 			}
 			else{
-				vehicle.speed += vehicle.acceleration; 
+				vehicle.x_speed += (vehicle.acceleration * Math.cos(vehicle.facing_direction))
+			}
+			if (Math.abs(vehicle.y_speed) >= Math.abs(vehicle.max_speed * Math.sin(vehicle.facing_direction))){
+				vehicle.y_speed -= Math.sign(vehicle.y_speed)-1 * (vehicle.acceleration * Math.sin(vehicle.facing_direction))
+			}
+			else{
+				vehicle.y_speed += -1 * (vehicle.acceleration * Math.sin(vehicle.facing_direction))
 			}
 		}
 		else{
-			if (vehicle.speed <= 0){
-				vehicle.speed = 0;
+			if(Math.abs(vehicle.x_speed) < 0.5){
+				vehicle.x_speed = 0
 			}
-			else{
-				vehicle.speed -= vehicle.acceleration;
+			if(Math.abs(vehicle.y_speed) < 0.5){
+				vehicle.y_speed = 0
 			}
 		}
 		if (counterclockwise){
@@ -67,10 +77,30 @@
 				vehicle.facing_direction -= vehicle.handling;
 			}
 		}
-		vehicle.x += (Math.round(vehicle.speed * Math.cos(vehicle.facing_direction)));
-		vehicle.y += -1 * (Math.round(vehicle.speed * Math.sin(vehicle.facing_direction)));		
-		
+		vehicle.x += vehicle.x_speed;
+		vehicle.y += vehicle.y_speed;
 		image_index = Math.round((vehicle.facing_direction * (180/Math.PI))/2);
+		if (bullet_timer > 0){
+		    bullet_timer -= 1
+		}else if (shooting && bullet_timer == 0){
+		    bullet = {
+				x:vehicle.x,
+				y:vehicle.y,
+				speed:70,
+				direction:vehicle.facing_direction,
+				image_index: Math.round((vehicle.facing_direction * (180/Math.PI))/2)%180
+				}
+				bullets.push(bullet)
+				bullet_timer = 15
+		}
+		for(var i = 0; i < bullets.length; i += 1){
+			bullets[i].x += Math.cos(bullets[i].direction)*bullets[i].speed
+			bullets[i].y += -1*Math.sin(bullets[i].direction)*bullets[i].speed
+			if  (bullets[i].x > width + 64 ||  bullets[i].x < -64 
+			    ||bullets[i].y > height + 64 || bullets[i].y < -64){
+				bullets.splice(i,1)
+			}    
+		}
 		if (image_index === 180){
 			image_index = 0;
 		}		
@@ -93,25 +123,33 @@
 	
 	function draw() {
 		context.clearRect(0,0,width,height);
+		for(var i = 0; i < bullets.length; i += 1){
+			context.drawImage(bullet_image,0 + 48*bullets[i].image_index,0,48,48,bullets[i].x-24,bullets[i].y-24,48,48);
+		}
 		context.drawImage(vehicle_image,0 + 64*image_index,0,64,64,vehicle.x-32,vehicle.y-32,64,64);
-		/*context.fillStyle = "#448844";
-		context.fillRect(vehicle.x - 25,vehicle.y - 15, 50, 30);*/
 	}
 	
 	function controls(event) {
 		key_code = event.keyCode;
 		if (key_code === 87 && accelerating === false){
-			console.log(0)
+			//w
 			accelerating = true;
 		}
 		else if (key_code === 65){
+			//a
 			counterclockwise = true;
 		}
 		else if (key_code === 68){
+			//d
 			clockwise = true;
 		}
 		else if (key_code === 83){
+			//s
 			reverse = true
+		}
+		else if (key_code === 32){
+			//space
+			shooting = true
 		}
 	}
 	
@@ -128,6 +166,10 @@
 		}
 		else if (key_code === 83){
 			reverse = false;
+		}
+		else if (key_code === 32){
+			
+			shooting = false
 		}
 	}
   
